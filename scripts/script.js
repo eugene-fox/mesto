60e6be89574f948d7ca49730438d1de9f7608aea
Eugene Korotkevich <eugene.korotkevich@yandex.ru>
2020-12-19 23:50:55 +0700

    Final edits before the second submission, JS bugs fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//объявляем необходимые константы
 const cardsContainerElement = document.querySelector('.galery__places');
 const templateElement = document.querySelector('.card-template');
 
@@ -22,15 +23,30 @@ const popupCloseButtons = document.querySelectorAll('.popup__close-button');
 const fullImageUrl = document.querySelector('.popup__image');
 const fullImageDesc = document.querySelector('.popup__image-title');
 
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
 
-//функция добавляет слушателей кнопок и карточки
-function addLikeDeleteView(card) {
-  const deleteButton = card.querySelector('.place-card__delete-button');
-  deleteButton.addEventListener('click', deleteCard);
-  const likeButton = card.querySelector('.place-card__like-button');
-  likeButton.addEventListener('click', toggleLike);
-  const cardsItem = card.querySelector('.place-card__photo');
-  cardsItem.addEventListener('click', openImagePopup);
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+//Функция открытие попапа с картинкой
+function openImagePopup(event) {
+  fullImageUrl.src = event.target.src;
+  fullImageUrl.alt = event.target.alt;
+  fullImageDesc.textContent = event.target.closest('.place-card').querySelector('.place-card__photo-name').textContent;
+  openPopup(addImagePopup);
+}
+
+//Функция удаление карточки
+function deleteCard(event) {
+  event.target.closest('.place-card').remove();
+}
+
+//Функция активации/деактивации лайка
+function toggleLike(event) {
+  event.target.classList.toggle('place-card__like-button_active');
 }
 
 //Функция создания карточки
@@ -44,18 +60,11 @@ function composeCard({
   cardTitle.textContent = name;
   cardImage.src = link;
   cardImage.alt = `Изображение на котором изображено место ${name}`;
-  addLikeDeleteView(newCard);
+  //Добавляем новой карточки обработчики клика на Удаление, Лайк и открытие картинки карточки
+  newCard.querySelector('.place-card__delete-button').addEventListener('click', deleteCard);
+  newCard.querySelector('.place-card__like-button').addEventListener('click', toggleLike);
+  newCard.querySelector('.place-card__photo').addEventListener('click', openImagePopup);
   return newCard;
-}
-
-//Удаление карточки
-function deleteCard(event) {
-  event.target.closest('.place-card').remove();
-}
-
-//Функция активации/деактивации лайка
-function toggleLike(event) {
-  event.target.classList.toggle('place-card__like-button_active');
 }
 
 //Функция отрисовки карточек
@@ -64,63 +73,56 @@ function renderCards() {
   cardsContainerElement.append(...cardsItems);
 }
 
-//Функция заполняет инпуты поп-апа редактирования профиля актуальными значениями
-function popupFill(name, description) {
-  name.value = profileName.textContent;
-  description.value = profileDescription.textContent;
-}
-
-addPopup.querySelector('.popup__save-button_card').addEventListener('click', (evt) => {
-  addImageCard();
-  popupVisible(evt);
-});
-
-function popupVisible(event) {
-  const targetPopup = event.target.closest('.popup')
-  targetPopup.classList.toggle('popup_opened');
-}
-
-popupCloseButtons.forEach((button) => {
-  button.addEventListener('click', popupVisible);
-});
-
-function openEditPopup() {
-  editPopup.classList.add('popup_opened');
-  popupFill(nameInput, jobInput);
-  editPopup.querySelector('.popup__container').addEventListener('submit', (evt) => {
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileDescription.textContent = jobInput.value;
-  });
-  editPopup.querySelector('.popup__save-button_profile').addEventListener('click', popupVisible);
-}
-
+//Данная функция должна обрабатывать сабмит формы добавления карточки.
 function addImageCard() {
   const newImageCard = composeCard({
     name: placeName.value,
     link: placeImageUrl.value
   });
   cardsContainerElement.prepend(newImageCard);
-  console.log(addPopup.querySelector('.popup__container'));
+
   addPopup.querySelector('.popup__container').reset();
 }
 
-function openAddPopup() {
-  addPopup.classList.add('popup_opened');
-  addPopup.querySelector('.popup__container').addEventListener('submit', (evt) => {
-    evt.preventDefault();
+popupCloseButtons.forEach((button) => {
+  button.addEventListener('click', (evt) =>{
+    const targetPopup = evt.target.closest('.popup');
+    closePopup(targetPopup);
   });
+});
+
+function openEditPopup() {
+  openPopup(editPopup);
+  //заполняем инпуты поп-апа редактирования профиля актуальными значениями
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDescription.textContent;
 }
 
-//Функция открытие попапа с картинкой
-function openImagePopup(event) {
-  fullImageUrl.src = event.target.src;
-  fullImageUrl.alt = event.target.alt;
-  fullImageDesc.textContent = event.target.closest('.place-card').querySelector('.place-card__photo-name').textContent;
-  addImagePopup.classList.add('popup_opened');
-}
-
+//Вешаем обработчики кнопки поп-апа редактирования и добавления
 editButton.addEventListener('click', openEditPopup);
-addButton.addEventListener('click', openAddPopup);
+addButton.addEventListener('click', () => {
+openPopup(addPopup);
+});
+
+//Вешаем обработчик на сабмит формы редактирования профиля
+editPopup.querySelector('.popup__container').addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+});
+
+//Вешаем обработчик на сабмит формы добавления карточки с фото
+addPopup.querySelector('.popup__container').addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  addImageCard();
+});
+
+//Вешаем обработчики на кнопки поп-апов, которые вызывают сабмит формы и в последсвии их закрывают
+editPopup.querySelector('.popup__save-button_profile').addEventListener('click', () => {
+  closePopup(editPopup);
+});
+addPopup.querySelector('.popup__save-button_card').addEventListener('click', () => {
+  closePopup(addPopup);
+});
 
 renderCards();
